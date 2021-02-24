1fda21df7a33ddf1b7522b0059f651d08ae759a6
Taichen <jatr812@gmail.com>
2021-02-24 07:52:40 -0800

    Played around with the home page and scroll reveal

--- js/main.js
+++ js/main.js
@@ -14,3 +14,21 @@ $(window).on("load",function() {
       });
     }).scroll(); //invoke scroll-handler on page-load
 });
+
+// Smooth Scroll Reveal Below
+overalDelay = 250;
+
+ScrollReveal().reveal('.background', {delay: overalDelay});
+ScrollReveal().reveal('.homeInfoName', {delay: overalDelay});
+ScrollReveal().reveal('.homeInfoP', {delay: overalDelay});
+ScrollReveal().reveal('.members', {delay: overalDelay});
+ScrollReveal().reveal('.repairCafe', {delay: overalDelay});
+ScrollReveal().reveal('.guides', {delay: overalDelay});
+ScrollReveal().reveal('.guideImg', {delay: overalDelay});
+ScrollReveal().reveal('.guideChunk', {delay: overalDelay});
+ScrollReveal().reveal('.repairCafeChunk', {delay: overalDelay});
+ScrollReveal().reveal('.repairCafeImg', {delay: overalDelay});
+ScrollReveal().reveal('.communityImg', {delay: overalDelay});
+ScrollReveal().reveal('.communityChunk', {delay: overalDelay});
+ScrollReveal().reveal('.moreInfo', {delay: overalDelay});
+ScrollReveal().reveal('.finalize', {delay: overalDelay});
