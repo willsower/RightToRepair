ffc0abf447bc4e4d38788072e7c8bf68925378bc
Taichen <jatr812@gmail.com>
2021-03-02 11:52:18 -0800

    Got community page somewhat finished

--- js/main.js
+++ js/main.js
@@ -38,6 +38,27 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
+  
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+// Inner Community CSS
+var acc = document.getElementsByClassName("discussionItemButton");
+var i;
+
+for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
